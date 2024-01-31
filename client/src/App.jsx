444d412c7c810b6a9a77acc42c54d0a98b3ec644
Senju002 <rajkaran.mei34@gmail.com>
2024-01-31 16:45:20 +0700

    header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,20 @@ import Dashboard from "./pages/Dashboard";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
+import Project from "./pages/Project";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/singin" element={<SignIn />} />
-        <Route path="/singup" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/projects" element={<Project />} />
       </Routes>
     </BrowserRouter>
   );
